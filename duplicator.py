ae1c746dbd3c3f289ce59d193a16d2652662d32d
Aubrey Gatewood <40611310+Xerxes979@users.noreply.github.com>
2022-02-07 13:55:54 -0500

    adding all changes from last week

--- duplicator.py
+++ duplicator.py
@@ -26,20 +26,6 @@ target = r'DummySheet2.xlsx'
 shutil.copyfile(original, target)
 #making a fresh sheet each time it runs
 
-"""
-#just clean duplicating the excel files
-path1 = "DummySheet.xlsx"
-path2 = "DummySheet2.xlsx"
-wb1 = xl.load_workbook(filename=path1)
-ws1 = wb1.worksheets[0]
-wb2 = xl.load_workbook(filename=path2)
-ws2 = wb2.create_sheet(ws1.title)
-for row in ws1:
-    for cell in row:
-        ws2[cell.coordinate].value = cell.value
-wb2.save(path2)
-#end duplicating code
-"""
 
 inputSheet = pd.read_excel('DummySheet.xlsx', sheet_name='Sheet1')
 outputSheet = pd.read_excel('DummySheet2.xlsx', sheet_name='Sheet1')
@@ -51,15 +37,24 @@ if file_exists:
 
 print("Column headings:")
 print(inputSheet.columns)
+dftry = []
+
 for i in inputSheet.index:
     if (len(str(inputSheet['GAUGE NUMBER'] [i])) > 5):
+        dftry.append(True)
         #print('need to do something here')
         print(inputSheet['GAUGE NUMBER'] [i])
+        #inputSheet = inputSheet.append(inputSheet[i]) #causes big problem
+
         #at this point, I need to duplicate the row, make the entry in the first slot
         #the first #, and the entry in the second slot the second number
+    else:
+        dftry.append(False)
 
-#check if file exists
-file_exists = exists('DummySheet2.xlsx')
-if file_exists:
-    print('file exists on line 62\n')
 
+for i in dftry:
+    if dftry[i]:
+        inputsheet.append(inputsheet[i])
+
+
+print(len(inputSheet))
