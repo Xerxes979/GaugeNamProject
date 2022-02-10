232def1bcefb4fda09d2e4c97d09f95869db8cd5
Aubrey Gatewood <40611310+Xerxes979@users.noreply.github.com>
2022-02-10 11:56:47 -0500

    made the row duplicator work for '#' '(' and '/' found in the gauge or nam number, and accounted for cases where they are in both to make sure that the row is not duplicated an extra time. Also worked on the value editor to make it edit for '/', '#', and '(', and made a separate string cut and concatenation for each of those characters. Next is adding edit functionality for nam numbers. So far it all works.

--- duplicator.py
+++ duplicator.py
@@ -50,12 +50,29 @@ print(inDF.columns)
 dftry = []
 
 #duplicating the rows, x is a var just to tell how many rows are duped
+#first part for rows with dashed gauge and nam numbers
+#second part for rows with nam XOR gauge
 x = 0
 for i in outDF.index:
-    if (("/" in (str(outDF['GAUGE NUMBER'] [i])))
+    if  ((("/" in (str(outDF['GAUGE NUMBER'] [i])))
         or ("(" in (str(outDF['GAUGE NUMBER'] [i])))
-        or ("#" in (str(outDF['GAUGE NUMBER'] [i])))): #this if actually works
+        or ("#" in (str(outDF['GAUGE NUMBER'] [i]))))
+        and 
+        (("/" in (str(outDF['NAM NUMBER '] [i])))
+        or ("(" in (str(outDF['NAM NUMBER '] [i])))
+        or ("#" in (str(outDF['NAM NUMBER '] [i]))))
+        ): #this if actually works
         #print(outDF['GAUGE NUMBER'] [i])
+        outDF.loc[len(outDF.index)] = outDF.loc[i]
+        x = x + 1
+    elif((("/" in (str(outDF['GAUGE NUMBER'] [i])))
+        or ("(" in (str(outDF['GAUGE NUMBER'] [i])))
+        or ("#" in (str(outDF['GAUGE NUMBER'] [i]))))
+        or 
+        (("/" in (str(outDF['NAM NUMBER '] [i])))
+        or ("(" in (str(outDF['NAM NUMBER '] [i])))
+        or ("#" in (str(outDF['NAM NUMBER '] [i]))))
+        ):
         outDF.loc[len(outDF.index)] = outDF.loc[i]
         x = x + 1
 print('x is ' + str(x))
@@ -71,6 +88,7 @@ outDF.sort_values(by=['GAUGE NUMBER'], ascending=True, inplace=True)
 #it literally just resets the indexes ... 
 outDF.reset_index(drop=True, inplace=True)
 
+#editing the values of gauge and nam numbers
 truth = 0
 for i in outDF.index:
     #think here: if 2 gauge numbers, if 2 nam numbers, if 2 of both, outliers ... 
@@ -83,18 +101,46 @@ for i in outDF.index:
             #print(outDF['GAUGE NUMBER'][i])
             #print(i)
             truth = 1
-    elif ("/" in (str(outDF['GAUGE NUMBER'] [i]))):
-        #print('in elif')
-        temp = outDF['GAUGE NUMBER'] [i]
-        temp2 = temp.split('/')[1]
-        outDF.loc[i,'GAUGE NUMBER'] = temp2
-        truth = 0
+        if ('#' in (str(outDF['GAUGE NUMBER'][i]))):
+            temp = outDF['GAUGE NUMBER'][i]
+            temp1=temp.split('#')[0]
+            outDF.loc[i,'GAUGE NUMBER'] = temp1
+            truth = 1
+        if ('(' in (str(outDF['GAUGE NUMBER'][i]))):
+            temp = outDF['GAUGE NUMBER'][i]
+            temp1=temp.split('(')[0]
+            outDF.loc[i,'GAUGE NUMBER'] = temp1
+            truth = 1
+    else:
+        if ("/" in (str(outDF['GAUGE NUMBER'] [i]))):
+            #print('in elif')
+            temp = outDF['GAUGE NUMBER'] [i]
+            temp2 = temp.split('/')[1]
+            outDF.loc[i,'GAUGE NUMBER'] = temp2
+            truth = 0
+        if ("#" in (str(outDF['GAUGE NUMBER'] [i]))):
+            #print('in elif')
+            temp = outDF['GAUGE NUMBER'] [i]
+            temp2 = temp.split('#')[0]
+            outDF.loc[i,'GAUGE NUMBER'] = temp2
+            truth = 0
+        if ('(' in (str(outDF['GAUGE NUMBER'][i]))):
+            temp = outDF['GAUGE NUMBER'][i]
+            temp1=temp.split('(')[0]
+            temp2=temp.split('(')[1]
+            temp2=temp2[:-1]
+            deletelength=len(temp2)
+            temp1=temp1[-deletelength]
+            temp1 = temp1 + temp2
+            outDF.loc[i,'GAUGE NUMBER'] = temp1
+            truth = 0
 
 
 
 #sorting by gauge number
 # outDF['GAUGE NUMBER'] = outDF['GAUGE NUMBER'].astype(str)
 # outDF.sort_values(by=['GAUGE NUMBER'], ascending=True, inplace=True)
+#for some reason commenting the above 2 lines made things work right ... not sure...
 outDF.reset_index(drop=True, inplace=True)
 
 #have to push the dataframe to the excel sheet for results to show
